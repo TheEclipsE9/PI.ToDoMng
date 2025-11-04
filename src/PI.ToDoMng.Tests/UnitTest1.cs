52c7bfe3b19937f0e247ef773c8bf13474ed3f6c
TheEclipsE9 <TheEclipsE@tut.by>
2025-11-04 21:20:11 +0100

    Mini Taask 1: Generate secure opaque token

---
+++ src/PI.ToDoMng.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace PI.ToDoMng.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- src/PI.ToDoMng.WebApi/Application/Utilities/OpaqueToken.cs
+++ src/PI.ToDoMng.WebApi/Application/Utilities/OpaqueToken.cs
@@ -1,9 +1,25 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace PI.ToDoMng.WebApi.Application.Utilities;
 
 public class OpaqueToken
 {
+    /// <summary>
+    /// Generates a secure random token of the specified length.
+    /// </summary>
+    /// <remarks>
+    /// Not URL-safe.
+    /// </remarks>
+    /// <param name="length">The number of random bytes to generate before Base64 encoding. Default is 32.</param>
+    /// <returns>Returns a Base64-encoded string suitable for use as an opaque session token.</returns>
     public static string GenerateToken(int length = 32)
     {
-        return "token";
+        using var generator = RandomNumberGenerator.Create();
+
+        byte[] bytes = new byte[length];
+        generator.GetBytes(bytes);
+
+        return Convert.ToBase64String(bytes);
     }
 }
